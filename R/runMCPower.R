1c9197b2ac56dc540ce8c2e307dbf89212964649
philchalmers <rphilip.chalmers@gmail.com>
2024-10-21 11:59:15 -0400

    make some more tweaks to logic

--- R/runMCPower.R
+++ R/runMCPower.R
@@ -8,10 +8,18 @@
 #' @param power power level to use. If set to \code{NA} then
 #' @param sig.level alpha level
 #' @param interval search interval to use when \code{\link{SimSolve}} is required
-#' @param Generate generate function (see \code{\link{runSimulation}})
+#' @param sim_function function that both creates the data and returns a single
+#'   p-value for the analysis of interest. Function must contain the arguments
+#'   \code{condition} to utilize the row in \code{conditions} and an
+#'   argument \code{fixed_objects} for catching any fixed object information
+#'   (see \code{\link{runSimulation}} for details)
 #' @param Analyse analyse function (see \code{\link{runSimulation}})
 #' @param replications number of replications to use when
 #'   \code{\link{runSimulation}} is required
+#' @param integer a logical value indicating whether the search iterations
+#'   use integers or doubles.
+#'   Automatically set to \code{FALSE} if \code{interval} contains
+#'   non-integer numbers, though in general this should be set explicitly
 #' @param ... additional parameters to pass to \code{\link{runSimulation}} or
 #'   \code{\link{SimSolve}}
 #'
@@ -20,34 +28,35 @@
 #' @examples
 #'
 #'
-runMCPower <- function(conditions, power, sig.level=.05,
-					   interval, Generate, Analyse, replications=10000, ...){
-	stopifnot(nrow(conditions) == 1)
+runMCPower <- function(conditions, sim_function, power, sig.level=.05,
+					   interval, replications=10000,
+					   integer, ...){
 	conditions$sig.level <- sig.level
 	if(missing(interval)) interval <- NA
+	if(missing(integer))
+		integer <- !has.decimals(interval)
 	if(missing(power)) power <- NA
+	sim_function_aug <- function(condition, dat, fixed_objects)
+		sim_function(condition=condition, fixed_objects=fixed_objects)
 	ret <- if(is.na(power)){
 		runSimulation(conditions, replications=replications,
-					  generate=Generate, analyse=Analyse,
+					  analyse=sim_function_aug,
 					  summarise=Internal_Summarise, ...)
 	} else {
 		  SimSolve(conditions, interval=interval,
-		  		 generate=Generate, analyse=Analyse,
-		  		 summarise=Internal_Summarise, b=power, ...)
+		  		 analyse=sim_function_aug,
+		  		 summarise=Internal_Summarise, b=power,
+		  		 integer=integer, ...)
 	}
 	ret
 }
 
-t.test_generate <- function(condition, fixed_objects) {
+t.test_sim <- function(condition, fixed_objects) {
 	Attach(condition)
 	group1 <- rnorm(N)
 	group2 <- rnorm(N, mean=d)
 	dat <- data.frame(group = gl(2, N, labels=c('G1', 'G2')),
 					  DV = c(group1, group2))
-	dat
-}
-
-t.test_analyse <- function(condition, dat, fixed_objects) {
 	p <- t.test(DV ~ group, dat, var.equal=TRUE)$p.value
 	p
 }
@@ -57,28 +66,31 @@ Internal_Summarise <- function(condition, results, fixed_objects) {
 	ret
 }
 
+has.decimals <- function(x){
+	intx <- as.integer(x)
+	any(abs(x - intx) > 0)
+}
+
 if(FALSE){
+
+	# estimate power given fixed inputs
+	runMCPower(createDesign(N = 50, d=.5),
+			   sim_function=t.test_sim)
 
 	# solve N to get 80% power
 	runMCPower(createDesign(N = NA, d = .5),
-			   Generate=t.test_generate,
-			   Analyse=t.test_analyse,
+			   sim_function=t.test_sim,
 			   power=.8, interval=c(2,500))
 
 	# solve d to get 80% power
 	runMCPower(createDesign(N = 50, d = NA),
-			   Generate=t.test_generate,
-			   Analyse=t.test_analyse,
-			   power=.8, interval=c(.1, 2), integer=FALSE)
-
-	# estimate power
-	runMCPower(createDesign(N = 50, d=.5),
-			   Generate=t.test_generate,
-			   Analyse=t.test_analyse)
+			   sim_function=t.test_sim,
+			   power=.8, interval=c(.1, 2))
 
 	# solve alpha
-	runMCPower(createDesign(N = 50, d=.5), interval=c(.0001, .8),
-			   Generate=t.test_generate, power=.8,
-			   Analyse=t.test_analyse, sig.level=NA, integer=FALSE)
+	runMCPower(createDesign(N = 50, d=.5),
+			   sim_function=t.test_sim,
+			   interval=c(.0001, .8),
+			   power=.8, sig.level=NA)
 }
 
