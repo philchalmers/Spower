02d4144c9edb57b96d080965d8025bd500112f39
philchalmers <rphilip.chalmers@gmail.com>
2024-10-23 16:10:20 -0400

    add paired information

--- R/p_sim_templates.R
+++ R/p_sim_templates.R
@@ -1,4 +1,4 @@
-#' Independent samples t-test simulation and p-value
+#' Independent/paired samples t-test simulation and p-value
 #'
 #' Function generates two sets of individuals with equal sample sizes
 #' and Cohen's effect size 'd'. The data and associated t-test
@@ -7,18 +7,22 @@
 #'
 #' @param n sample size per group, assumed equal across groups
 #' @param d Cohen's standardized effect size
+#' @param paired logical; should the analysis results assume that the groups
+#'   are dependent?
 #' @return a single p-value
 #' @examples
 #'
 #' # sample size of 50 per group, medium effect size
 #' p_t.test(n=50, d=0.5)
+#' p_t.test(n=50, d=0.5, paired=TRUE)
 #'
 #' @export
-p_t.test <- function(n, d) {
+p_t.test <- function(n, d, paired = FALSE) {
 	group1 <- rnorm(n)
 	group2 <- rnorm(n, mean=d)
 	dat <- data.frame(group = gl(2, n, labels=c('G1', 'G2')),
 					  DV = c(group1, group2))
-	p <- t.test(DV ~ group, dat, var.equal=TRUE)$p.value
+	p <- if(paired) t.test(group1, group2, paired=TRUE)$p.value
+		 else t.test(DV ~ group, dat, var.equal=TRUE)$p.value
 	p
 }
