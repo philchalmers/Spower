dd86498b719006d64df5c579ac1c338a67d1392d
philchalmers <rphilip.chalmers@gmail.com>
2024-10-26 12:02:40 -0400

    use type for switching

--- R/p_sim_templates.R
+++ R/p_sim_templates.R
@@ -8,12 +8,12 @@
 #' @param n sample size per group, assumed equal across groups
 #' @param d Cohen's standardized effect size \code{d}
 #' @param mu population mean to test against
-#' @param paired logical; should the analysis results assume that the groups
-#'   are dependent?
+#' @param type type of t-test to use; can be \code{'two.sample'},
+#'   \code{'one.sample'}, or \code{'paired'}
 #' @param two.tailed logical; should a two-tailed or one-tailed test be used?
 #' @param n2_n1 allocation ratio reflecting the same size ratio.
 #'   Default of 1 sets the groups to be the same size. Only applicable
-#'   when \code{paired = FALSE}
+#'   when \code{type = 'two.sample'}
 #' @param raw_info (optional) list of mean and variance inputs for each group.
 #'   If supplied the \code{d} input will be ignored
 #' @return a single p-value
@@ -21,16 +21,21 @@
 #'
 #' # sample size of 50 per group, medium effect size
 #' p_t.test(n=50, d=0.5)
-#' p_t.test(n=50, d=0.5, paired=TRUE)
 #'
 #' # second group 2x as large as the first group
 #' p_t.test(n=50, d=0.5, n2_n1 = 2)
 #'
+#' # paired and one-sample tests
+#' p_t.test(n=50, d=0.5, type = 'paired')
+#' p_t.test(n=50, d=0.5, type = 'one.sample')
+#'
 #' @export
-p_t.test <- function(n, d, mu = 0, paired = FALSE,
+p_t.test <- function(n, d, mu = 0,
+					 type = c('two.sample', 'one.sample', 'paired'),
 					 n2_n1 = 1, two.tailed = TRUE,
 					 raw_info = list(mu1=NA, mu2=NA,
 					 				sigma1=NA, sigma2=NA)) {
+	type <- match.arg(type)
 	if(!is.na(raw_info$mu1)){
 		group1 <- with(raw_info, rnorm(n, mean=mu1, sd=sigma1))
 		group2 <- with(raw_info, rnorm(n * n2_n1, mean=mu2, sd=sigma2))
@@ -40,10 +45,15 @@ p_t.test <- function(n, d, mu = 0, paired = FALSE,
 	}
 	dat <- data.frame(group = factor(rep(c('G1', 'G2'), times=c(n, n*n2_n1))),
 					  DV = c(group1, group2))
-	p <- if(paired){
+	p <- if(type == 'paired'){
 		if(n2_n1 != 1) stop('n2_n1 must equal 1 for paired t-tests')
 		t.test(group1, group2, mu=mu, paired=TRUE)$p.value
-	} else t.test(DV ~ group, dat, var.equal=TRUE, mu=mu)$p.value
+	} else if(type == 'two.sample'){
+		t.test(DV ~ group, dat, var.equal=TRUE, mu=mu)$p.value
+	} else if(type == 'one.sample') {
+		dv <- rnorm(n, mean=d)
+		t.test(dv, mu=mu)$p.value
+	}
 	p <- ifelse(two.tailed, p, p/2)
 	p
 }
