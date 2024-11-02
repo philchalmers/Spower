4fcaa1721cf0b2030c468117c615345097af3a9f
philchalmers <rphilip.chalmers@gmail.com>
2024-11-02 13:05:39 -0400

    add mcnemar.test

--- R/p_sim_templates.R
+++ R/p_sim_templates.R
@@ -237,6 +237,47 @@ p_prop.test <- function(n, h, prop, pi = .5,
 	p
 }
 
+#' McNemar test simulation and p-value
+#'
+#' Generates two-dimensional sample data for McNemar test and
+#' return a p-value. Uses \code{\link{mcnemar.test}}.
+#'
+#' @param n total sample size
+#' @param prop two-dimensional matrix of proportions/probabilities
+#' @param two.tailed logical; should a two-tailed or one-tailed test be used?
+# @param exact logical; use fisher's exact test via \code{\link{fisher.test}}?
+#' @param correct logical; use continuity correction? Only applicable for
+#'   2x2 tables
+#' @return a single p-value
+#' @examples
+#'
+#' # from ?mcnemar.test
+#' Performance <- matrix(c(794, 86, 150, 570),
+#' 		   nrow = 2,
+#' 		   dimnames = list("1st Survey" = c("Approve", "Disapprove"),
+#' 		               "2nd Survey" = c("Approve", "Disapprove")))
+#' (prop <- prop.table(Performance))
+#'
+#' # one sample + test and resulting p-value
+#' p_mcnemar.test(n=sum(Performance), prop=prop)
+#'
+#' \dontrun{
+#'
+#' # post-hoc power (not recommended)
+#' Spower(p_mcnemar.test, n=sum(Performance), prop=prop)
+#'
+#' }
+#'
+#' @export
+p_mcnemar.test <- function(n, prop,
+						   two.tailed = TRUE, correct=TRUE) {
+	draws <- rmultinom(1, n, prob = as.numeric(prop))
+	p <- mcnemar.test(matrix(draws, nrow(prop), ncol(prop)),
+					  correct=correct)$p.value
+	p <- ifelse(two.tailed, p, p/2)
+	p
+}
+
 #' One-way ANOVA simulation and p-value
 #'
 #' Generates continuous multi-sample data to be analysed by
