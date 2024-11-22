2eae8ee9b834a09d6603931ea427c139ad304202
philchalmers <rphilip.chalmers@gmail.com>
2024-11-21 21:09:32 -0500

    add docs

--- R/p_sim_templates_Gpower.R
+++ R/p_sim_templates_Gpower.R
@@ -575,7 +575,7 @@ p_wilcox.test <- function(n, d, n2_n1 = 1, mu=0,
 	p
 }
 
-# Linear model
+# Linear model and p-value
 #
 #
 #
@@ -598,57 +598,73 @@ p_glm <- function(n){
 
 }
 
-#' Compare two correlations
+#' Compare two correlations and p-value
 #'
-#'
+#' @param n sample size
+#' @param r.ab1 correlation between variable A and B in sample 1
+#' @param r.ab2 correlation between variable A and B in sample 2
+#' @param r.ac1 same pattern as \code{r.ab1}
+#' @param r.ac2 same pattern as \code{r.ab2}
+#' @param r.bc1 ...
+#' @param r.bc2 ...
+#' @param r.ad1 ...
+#' @param r.ad2 ...
+#' @param r.bd1 ...
+#' @param r.bd2 ...
+#' @param r.cd1 ...
+#' @param r.cd2 ...
+#' @param n2_n1 sample size ratio
+#' @param two.tailed logical; use two-tailed test?
+#' @param type type of correlation design
+#' @param test hypothesis method to use. Defaults to 'fisher1925'
 #' @export
 #' @examples
 #'
 #' # independent (same x-y pairing across groups)
-#' p_2r(100, r.xy1=.5, r.xy2=.6)
+#' p_2r(100, r.ab1=.5, r.ab2=.6)
 #'
 #' # overlap (same y, different xs)
-#' p_2r(100, r.xy1=.5, r.xy2=.7,
-#'           r.xz1=.3, r.xz2=.3,
-#'           r.yz1=.2, r.yz2=.2, type = 'overlap')
+#' p_2r(100, r.ab1=.5, r.ab2=.7,
+#'           r.ac1=.3, r.ac2=.3,
+#'           r.bc1=.2, r.bc2=.2, type = 'overlap')
 #'
 #' # nonoverlap (different ys, different xs)
-#' p_2r(100, r.xy1=.5, r.xy2=.6,
-#'           r.xz1=.3, r.xz2=.3,
-#'           r.yz1=.2, r.yz2=.2,
-#'           r.xw1=.2, r.xw2=.2,
-#'           r.yw1=.4, r.yw2=.4,
-#'           r.zw1=.2, r.zw2=.2,
+#' p_2r(100, r.ab1=.5, r.ab2=.6,
+#'           r.ac1=.3, r.ac2=.3,
+#'           r.bc1=.2, r.bc2=.2,
+#'           r.ad1=.2, r.ad2=.2,
+#'           r.bd1=.4, r.bd2=.4,
+#'           r.cd1=.2, r.cd2=.2,
 #'           type = 'nonoverlap')
 #'
 #'
-p_2r <- function(n, r.xy1, r.xy2, r.xz1, r.xz2, r.yz1, r.yz2,
-				 r.xw1, r.xw2, r.yw1, r.yw2, r.zw1, r.zw2,
+p_2r <- function(n, r.ab1, r.ab2, r.ac1, r.ac2, r.bc1, r.bc2,
+				 r.ad1, r.ad2, r.bd1, r.bd2, r.cd1, r.cd2,
 				 n2_n1 = 1, two.tailed=TRUE,
 				 type = c('independent', 'overlap', 'nonoverlap'),
 				 test = 'fisher1925'){
 	type <- match.arg(type)
 	if(type == 'independent'){
-		R1 <- matrix(c(1,r.xy1, r.xy1, 1), 2, 2)
-		R2 <- matrix(c(1,r.xy2, r.xy2, 1), 2, 2)
+		R1 <- matrix(c(1,r.ab1, r.ab1, 1), 2, 2)
+		R2 <- matrix(c(1,r.ab2, r.ab2, 1), 2, 2)
 		cnms <- c('y', 'x')
 	} else if(type == 'overlap'){
-		R1 <- matrix(c(1,r.xy1, r.xz1,
-					   r.xy1, 1, r.yz1,
-					   r.xz1,r.yz1, 1), 3, 3)
-		R2 <- matrix(c(1,r.xy2, r.xz2,
-					   r.xy2, 1, r.yz2,
-					   r.xz2, r.yz2, 1), 3, 3)
+		R1 <- matrix(c(1,r.ab1, r.ac1,
+					   r.ab1, 1, r.bc1,
+					   r.ac1,r.bc1, 1), 3, 3)
+		R2 <- matrix(c(1,r.ab2, r.ac2,
+					   r.ab2, 1, r.bc2,
+					   r.ac2, r.bc2, 1), 3, 3)
 		cnms <- c('y', 'x1', 'x2')
 	} else {
-		R1 <- matrix(c(1,r.xy1, r.xz1, r.xw1,
-					   0, 1, r.yz1, r.yw1,
-					   0,0, 1, r.zw1,
+		R1 <- matrix(c(1,r.ab1, r.ac1, r.ad1,
+					   0, 1, r.bc1, r.bd1,
+					   0,0, 1, r.cd1,
 					   0, 0, 0, 1), 4, 4)
 		R1 <- R1 + t(R1) - diag(4)
-		R2 <- matrix(c(1,r.xy2, r.xz2, r.xw2,
-					   0, 1, r.yz2, r.yw2,
-					   0,0, 1, r.zw2,
+		R2 <- matrix(c(1,r.ab2, r.ac2, r.ad2,
+					   0, 1, r.bc2, r.bd2,
+					   0,0, 1, r.cd2,
 					   0, 0, 0, 1), 4, 4)
 		R2 <- R2 + t(R2) - diag(4)
 		cnms <- c('y1', 'x1', 'y2', 'x2')
