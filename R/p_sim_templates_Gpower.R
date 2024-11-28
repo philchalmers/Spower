6b946d1e5d053cd496ace3d7a20a1af492a6ccf1
philchalmers <rphilip.chalmers@gmail.com>
2024-11-28 09:19:15 -0500

    stop random.X

--- R/p_sim_templates_Gpower.R
+++ R/p_sim_templates_Gpower.R
@@ -605,21 +605,24 @@ p_lm <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0, R2.resid=1-R2, fixed.X=TRUE){
 			x[floor(seq(1, nrow(x), length.out=n)),]
 		X <- X[1:n, ]
 		X <- scale(X)
+		colnames(X) <- paste0('X', 1:k)
 	}
 	if(k.R2_0 == 0)
 		stopifnot(k >= 2)
-	if(k.R2_0 > 0 || R2_0 == 0){
-		R2s <- R2 - R2_0
-		betas <- c(sqrt(R2s), sqrt(R2_0), rep(0, k-2))
-		y <- colSums(betas * t(X)) + rnorm(n, 0, sqrt(R2.resid))
-		df <- data.frame(y, X)
-		if(k.R2_0 > 0)
-			df2 <- df[,c(1,3:(k.R2_0 +2))]
-		mod1 <- lm(y ~ ., df)
+	R2s <- R2 - R2_0
+	betas <- c(sqrt(R2s), sqrt(R2_0), rep(0, k-2))
+	y <- colSums(betas * t(X)) + rnorm(n, 0, sqrt(R2.resid))
+	df <- data.frame(y, X)
+	if(k.R2_0 > 0)
+		df2 <- df[,c(1,3:(k.R2_0 +2))]
+	mod1 <- lm(y ~ ., df)
+	if(!fixed.X && k.R2_0 == 0){
+		stop('Random X with non-zero R2_0 not currently supported', call.=FALSE)
+	} else {
 		mod0 <- if(R2_0 == 0) lm(y ~ 1, df)
 		  else lm(y ~ ., df2)
-		p <- anova(mod0, mod1)[2, "Pr(>F)"]
 	}
+	p <- anova(mod0, mod1)[2, "Pr(>F)"]
 	p
 }
 
@@ -636,11 +639,11 @@ if(FALSE){
 
 	# Example 7.3b
 	# G*power gives 0.3464 (broken)
-	Spower(p_lm, n=100, R2=.4, R2_0 = .3, k=5)
+	Spower(p_lm, n=100, R2=.4, R2_0 = .3, k=5, fixed.X=FALSE)
 
 	# Example 7.3c
 	# G*power gives N=153 (broken)
-	Spower(p_lm, n=NA, R2=.05, R2_0 = .2, k=5, power=.9,
+	Spower(p_lm, n=NA, R2=.05, R2_0 = .2, k=5, fixed.X=FALSE, power=.9,
 		   interval=c(50,300))
 
 
