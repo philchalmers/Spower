2dae9ade285d384107564dd499849f0f77a72e92
philchalmers <rphilip.chalmers@gmail.com>
2024-12-19 11:46:13 -0500

    update power after collected

--- R/update_functions.R
+++ R/update_functions.R
@@ -13,7 +13,7 @@
 #' @examples
 #' \dontrun{
 #' # Solve beta/alpha ratio to specific error trade-off constant
-#' (out <- Spower(p_t.test, n = 50, d = .5, beta_alpha = 2))
+#' out <- Spower(p_t.test, n = 50, d = .5, beta_alpha = 2)
 #'
 #' # update beta_alpha criteria without re-simulating
 #' updateCompromise(out, beta_alpha=4)
@@ -44,22 +44,37 @@ updateCompromise <- function(x, beta_alpha){
 #' @param x object returned from \code{\link{Spower}} where \code{power}
 #'   was estimated or the \code{bete_alpha} criteria were supplied
 #' @param sig.level Type I error rate (alpha)
+#' @param predCI confidence interval precision (see \code{\link{Spower}} for
+#'   similiar input)
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #'
 #' # estimate power using sig.level = .05 (default)
-#' (out <- Spower(p_t.test, n = 50, d = .5))
+#' out <- Spower(p_t.test, n = 50, d = .5)
 #'
-#' # update power estimate given sig.level = .01
+#' # update power estimate given sig.level=.01 and .20
 #' update_sig.level(out, sig.level=.01)
+#' update_sig.level(out, sig.level=.20)
 #'
 #' }
 #'
-update_sig.level <- function(x, sig.level){
+update_sig.level <- function(x, sig.level, predCI=.95){
 	if(missing(sig.level))
 		stop('Must specify sig.level')
-	stopifnot(sig.level < 0 || sig.level > 1)
-	browser()
+	stopifnot(sig.level > 0 && sig.level < 1)
+	x$sig.level <- attr(x, "Spower_extra")$conditions$sig.level <- sig.level
+	out <- SimDesign::reSummarise(Internal_Summarise, results=x)
+	design_names <- attr(x, 'design_names')$design
+	x$power <- as.numeric(out[,length(design_names)+1])
+	replications <- x$REPLICATIONS
+	alpha <- 1 - predCI
+	CI <- x$power + c(qnorm(c(alpha/2, predCI+alpha/2))) *
+		sqrt((x$power * (1-x$power))/replications)
+	CI[CI > 1] <- 1
+	CI[CI < 0] <- 0
+	names(CI) <- paste0('CI_', c(alpha/2, predCI+alpha/2)*100)
+	attr(x, 'extra_info')$power.CI <- CI
+	x
 }
