170592788752923ebc907cfbeb83a93d0be8accf
philchalmers <rphilip.chalmers@gmail.com>
2025-03-17 11:45:08 -0400

    random model feels odd, remove for now

--- R/p_lm.R2.R
+++ R/p_lm.R2.R
@@ -1,7 +1,8 @@
 #' p-value from global linear regression model simulation
 #'
-#' p-values associated with linear regression model using fixed or random
-#' independent variables.
+#' p-values associated with linear regression model using fixed
+#' independent variables. Focus is on the omnibus behavior of the
+#' R^2 statistic.
 #'
 #' @param n sample size
 #' @param R2 R-squared effect size
@@ -11,22 +12,22 @@
 #' @param R2.resid residual R-squared value, typically used when comparing
 #'   nested models when fit sequentially (e.g., comparing model A vs B when
 #'   model involves the structure A -> B -> C)
-#' @param fixed.X logical; should the IVs be considered fixed or random?
-#' @param gen_fun function used to generate the required discrete data.
-#'   Object returned must be a \code{data.frame}. Default uses \code{\link{gen_lm.R2}}.
-#'   User defined version of this function must include the argument \code{...}
+# @param gen_fun function used to generate the required discrete data.
+#   Object returned must be a \code{data.frame}. Default uses \code{\link{gen_lm.R2}}.
+#   User defined version of this function must include the argument \code{...}
 #' @param ... additional arguments to be passed to \code{gen_fun}. Not used
 #'   unless a customized \code{gen_fun} is defined
-#' @seealso \code{\link{p_glm}}, \code{\link{gen_lm.R2}}
+#' @seealso \code{\link{p_glm}}
 #' @export
 #' @examples
 #'
 #' # 5 fixed IVs, R^2 = .1, sample size of 95
 #' p_lm.R2(n=95, R2=.1, k=5)
 #'
-p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0,
-				 R2.resid=1-R2, fixed.X=TRUE, gen_fun=gen_lm.R2, ...){
-	stopifnot(R2 > R2_0)
+p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0, R2.resid=1-R2, ...){
+	stopifnot(R2 >= R2_0)
+	gen_fun <- gen_lm.R2
+	fixed.X <- TRUE
 	if(k.R2_0 == 0)
 		stopifnot(k >= 2)
 	df <- gen_fun(n=n, fixed.X=fixed.X, k=k,
@@ -34,12 +35,10 @@ p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0,
 	if(k.R2_0 > 0)
 		df2 <- df[,c(1,3:(k.R2_0 + 2))]
 	mod1 <- lm(y ~ ., df)
-	if(!fixed.X && k.R2_0 == 0 && R2_0 != 0){
+	if(!fixed.X && k.R2_0 == 0 && R2_0 != 0)
 		stop('Random X with non-zero R2_0 not currently supported', call.=FALSE)
-	} else {
-		mod0 <- if(R2_0 == 0) lm(y ~ 1, df)
-		else lm(y ~ ., df2)
-	}
+	mod0 <- if(R2_0 == 0) lm(y ~ 1, df)
+	else lm(y ~ ., df2)
 	p <- anova(mod0, mod1)[2, "Pr(>F)"]
 	p
 }
