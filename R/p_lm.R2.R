bf8271690ae64b57d2b9b0a72b39f5d95d0d64aa
philchalmers <rphilip.chalmers@gmail.com>
2025-03-24 10:12:51 -0400

    allow random model in a limited sense for now

--- R/p_lm.R2.R
+++ R/p_lm.R2.R
@@ -1,6 +1,6 @@
 #' p-value from global linear regression model simulation
 #'
-#' p-values associated with linear regression model using fixed
+#' p-values associated with linear regression model using fixed/random
 #' independent variables. Focus is on the omnibus behavior of the
 #' R^2 statistic.
 #'
@@ -12,6 +12,8 @@
 #' @param R2.resid residual R-squared value, typically used when comparing
 #'   nested models when fit sequentially (e.g., comparing model A vs B when
 #'   model involves the structure A -> B -> C)
+#' @param fixed logical; if FALSE then the data are random generated
+#'   according to a joint multivariate normal distribution
 # @param gen_fun function used to generate the required discrete data.
 #   Object returned must be a \code{data.frame}. Default uses \code{\link{gen_lm.R2}}.
 #   User defined version of this function must include the argument \code{...}
@@ -26,7 +28,13 @@
 #' # 5 fixed IVs, R^2 = .1, sample size of 95
 #' p_lm.R2(n=95, R2=.1, k=5)
 #'
-p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0, R2.resid=1-R2, ...){
+#' # random model
+#' p_lm.R2(n=95, R2=.1, k=5, fixed=FALSE)
+#'
+p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0, R2.resid=1-R2, fixed=TRUE,
+					...){
+	if(!fixed)
+		return(p_lm.R2.random(n=n, R2=R2, k=k, R2_0=R2_0))
 	stopifnot(R2 >= R2_0)
 	fixed.X <- TRUE
 	gen_fun <- gen_lm.R2
@@ -45,16 +53,33 @@ p_lm.R2 <- function(n, R2, k, R2_0 = 0, k.R2_0 = 0, R2.resid=1-R2, ...){
 	p
 }
 
+p_lm.R2.random <- function(n, R2, k, R2_0 = 0) {
+	stopifnot("Non-zero R2_0 values not yet supported for random model" = R2_0 == 0)
+	Sigma <- matrix(0, nrow = k + 1, ncol = k + 1)
+	Sigma[1,2] <- Sigma[2,1] <- sqrt(R2)
+	diag(Sigma) <- 1
+	data <- SimDesign::rmvnorm(n, sigma = Sigma)
+	y <- data[, 1]
+	X <- data[, -1]
+	model <- lm(y ~ X)
+	f_values <- summary(model)$fstatistic
+	p <- unname(pf(f_values[1], f_values[2], f_values[3], lower.tail = FALSE))
+	p
+}
+
 if(FALSE){
+
+	# correct (gives G*Power gives .6626)
+	p_lm.R2(n=95, R2=.1, R2_0 = .0, k=5, fixed=FALSE) |> Spower()
 
 	# Example 7.3b
 	# G*power gives 0.3464 (broken)
-	p_lm.R2(n=100, R2=.4, R2_0 = .3, k=5, k.R2_0=1, fixed.X=FALSE) |>
+	p_lm.R2(n=100, R2=.4, R2_0 = .3, k=5, fixed=FALSE) |>
 		Spower()
 
 	# Example 7.3c
 	# G*power gives N=153 (broken)
-	p_lm.R2(n=NA, R2=.05, R2_0 = .2, k=5, fixed.X=FALSE) |>
+	p_lm.R2(n=NA, R2=.05, R2_0 = .2, k=5, fixed=FALSE) |>
 		Spower(power=.9, interval=c(50,300))
 
 
