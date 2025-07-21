e9b5233e0d0017a7615aa4f5d7cddfaa619fa941
philchalmers <rphilip.chalmers@gmail.com>
2025-07-21 09:12:03 -0400

    add example

---
+++ dev/TOSTER_Spower.R
@@ -0,0 +1,59 @@
+# Equivalence test example using TOSTER and Spower
+library(Spower)
+library(TOSTER)
+
+equiv.t.test <- function(n, m1, sd1, m2, sd2, LB, UB){
+	g1 <- rnorm(n, m1, sd1)
+	g2 <- rnorm(n, m2, sd2)
+	out <- tsum_TOST(m1=mean(g1), sd1=sd(g1),
+			  m2=mean(g2), sd2=sd(g2),
+			  n1=n, n2=n,
+			  low_eqbound=LB,
+			  high_eqbound=UB,
+			  eqbound_type = "raw",
+			  paired = FALSE) # This is quite slow due to extra computations
+	out$TOST[2,]$p.value < .05/2 & out$TOST[3,]$p.value < .05/2
+}
+
+# These are equivalent
+equiv.t.test(n=100, m1=12, m2=11, sd1=2.5, sd2=2.5, LB=-2.5, UB=2.5) |>
+	Spower(replications=1000)
+
+power_t_TOST(n = 100,
+			 delta = 1,
+			 sd = 2.5,
+			 eqb = 2.5,
+			 alpha = .025,
+			 power = NULL,
+			 type = "two.sample")
+
+# solve sample size
+equiv.t.test(n=NA, m1=12, m2=11, sd1=2.5, sd2=2.5, LB=-2.5, UB=2.5) |>
+	Spower(power=.95, interval=c(50, 200))
+
+power_t_TOST(n = NULL,
+			 delta = 1,
+			 sd = 2.5,
+			 eqb = 2.5,
+			 alpha = .025,
+			 power = .95,
+			 type = "two.sample")
+
+
+######################
+
+# Using CI approach (much faster)
+CI.equiv.t.test <- function(n, m1, sd1, m2, sd2, LB, UB, conf.level=.95){
+	g1 <- rnorm(n, m1, sd1)
+	g2 <- rnorm(n, m2, sd2)
+	CI <- t.test(g2, g1, var.equal=TRUE,
+				 conf.level=conf.level)$conf.int
+	LB < CI[1] && CI[2] < UB
+}
+
+CI.equiv.t.test(n=100, m1=12, m2=11, sd1=2.5, sd2=2.5, LB=-2.5, UB=2.5) |>
+	Spower(replications=1000)
+
+# solve sample size
+CI.equiv.t.test(n=NA, m1=12, m2=11, sd1=2.5, sd2=2.5, LB=-2.5, UB=2.5) |>
+	Spower(power=.95, interval=c(50, 200))
