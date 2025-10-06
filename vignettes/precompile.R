04047ed051d279994ae4c691038b1b0ab5c5aa3d
philchalmers <rphilip.chalmers@gmail.com>
2025-10-06 15:22:08 -0400

    better automate precompile flow

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -3,3 +3,8 @@
 library(knitr)
 setwd('vignettes')
 knit("original/gpower_examples.Rmd", "gpower_examples.Rmd")
+
+Sys.setenv(SPOWER_EVAL = TRUE)
+rmarkdown::render("SpowerIntro.Rmd")
+rmarkdown::render("SpowerIntro_logicals.Rmd")
+SimDesign::SimClean("SpowerIntro.html", "SpowerIntro_logicals.html")
