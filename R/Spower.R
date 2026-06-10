82c7a06132a2d6d8cf265ca609b2024ec6a6e544
philchalmers <rphilip.chalmers@gmail.com>
2026-06-10 10:12:51 -0400

    don't expand objects unintentionally

--- R/Spower.R
+++ R/Spower.R
@@ -534,13 +534,14 @@ Spower <- function(..., power = NA, sig.level=.05, interval,
 		stop('Exactly *one* argument must be set to \'NA\' in Spower(..., power, sig.level)',
 			 call.=FALSE)
 	lst_expr <- as.list(expr)[-1]
-	if(length(lst_expr))
-		lst_expr <- lst_expr[sapply(lst_expr, \(x) is.atomic(x) || is.list(x) || is.call(x))]
 	if(length(lst_expr) && any(sapply(lst_expr, is.call))){
-		lst_expr[sapply(lst_expr, is.call)] <- sapply(lst_expr[sapply(lst_expr, is.call)], eval)
-		lst_expr[sapply(lst_expr, \(x) is(x, 'formula'))] <-
-			sapply(lst_expr[sapply(lst_expr, \(x) is(x, 'formula'))], deparse1)
+		tmp <- lst_expr
+		tmp[sapply(lst_expr, is.call)] <- sapply(tmp[sapply(lst_expr, is.call)], eval)
+		lst_expr[sapply(tmp, \(x) is(x, 'formula'))] <-
+			sapply(tmp[sapply(tmp, \(x) is(x, 'formula'))], deparse1)
 	}
+	if(length(lst_expr))
+		lst_expr <- lst_expr[sapply(lst_expr, \(x) is.atomic(x) || is.list(x))]
 	conditions <- do.call(SimDesign::createDesign, c(lst_expr, sig.level=sig.level, power=power))
 	if(missing(interval)){
 		if(is.na(sig.level) || length(fixed_objects$pick))
